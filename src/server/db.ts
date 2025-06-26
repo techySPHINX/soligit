d66ddef3202d5718911736a2a7e33302a2a88a5b
techySphinx <138506773+techySPHINX@users.noreply.github.com>
2025-06-27 01:21:10 +0530

    feat : basic setup

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,17 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 
-import { env } from "~/env";
-
-const createPrismaClient = () =>
-  new PrismaClient({
-    log:
-      env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-  });
+import { env } from "../env.mjs";
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: ReturnType<typeof createPrismaClient> | undefined;
+  prisma: PrismaClient | undefined;
 };
 
-export const db = globalForPrisma.prisma ?? createPrismaClient();
+export const db =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
+  });
 
 if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
